4d400e038b541aeb59f55c1d8730ab265f12a260
DaryaNaymon <darya.naymon@gmail.com>
2023-01-22 19:17:21 -0500

    add output to file

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,27 +22,38 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         date_list.append(row[0])
         profit_loss_list.append(int(row[1]))
-# The total number of months included in the dataset
-print("total month "+str(len(date_list)))
-# The net total amount of "Profit/Losses" over the entire period
-print("total $"+str(sum(profit_loss_list)))
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
-change_list = []
-for previous, next in zip(profit_loss_list,profit_loss_list[1:]):
-    change_list.append(next-previous)
-print("Average Change $"+ str(round(sum(change_list)/len(change_list),2)))
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in profits (date and amount) over the entire period
-min_val = change_list[0]
-max_val = change_list[0]
-min_idx = 0
-max_idx = 0
-for i,change in enumerate(change_list[1:],1):
-    if change >max_val:
-        max_val = change
-        max_idx = i
-    if change <min_val:
-        min_val = change
-        min_idx = i
-print("Greatest Increase in Profits " + date_list[max_idx] +" $"+ str(max_val))
-print("Greatest Decrease in Profits " + date_list[min_idx] +" $"+ str(min_val))
+with open("Analysis/results.txt","w") as result_file:
+    # The total number of months included in the dataset
+    print("total month "+str(len(date_list)))
+    result_file.write("total month "+str(len(date_list)))
+    result_file.write("\n")
+    # The net total amount of "Profit/Losses" over the entire period
+    print("total $"+str(sum(profit_loss_list)))
+    result_file.write("total $"+str(sum(profit_loss_list)))
+    result_file.write("\n")
+    # The changes in "Profit/Losses" over the entire period, and then the average of those changes
+    change_list = []
+    for previous, next in zip(profit_loss_list,profit_loss_list[1:]):
+        change_list.append(next-previous)
+    print("Average Change $"+ str(round(sum(change_list)/len(change_list),2)))
+    result_file.write("Average Change $"+ str(round(sum(change_list)/len(change_list),2)))
+    result_file.write("\n")
+    # The greatest increase in profits (date and amount) over the entire period
+    # The greatest decrease in profits (date and amount) over the entire period
+    min_val = change_list[0]
+    max_val = change_list[0]
+    min_idx = 0
+    max_idx = 0
+    for i,change in enumerate(change_list[1:],1):
+        if change >max_val:
+            max_val = change
+            max_idx = i
+        if change <min_val:
+            min_val = change
+            min_idx = i
+    print("Greatest Increase in Profits " + date_list[max_idx] +" $"+ str(max_val))
+    result_file.write("Greatest Increase in Profits " + date_list[max_idx] +" $"+ str(max_val))
+    result_file.write("\n")
+    print("Greatest Decrease in Profits " + date_list[min_idx] +" $"+ str(min_val))
+    result_file.write("Greatest Decrease in Profits " + date_list[min_idx] +" $"+ str(min_val))
+    result_file.write("\n")
